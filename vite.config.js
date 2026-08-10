5caefa0916ac8922056e7a352ce150a26410ebba
madscientist-314 <tomkirby314@gmail.com>
2026-08-10 23:54:47 +0100

    Vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // REPLACE 'gel-cutting-engine' with your exact GitHub repository name!
   base: '/Gel Cutter/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
 });
